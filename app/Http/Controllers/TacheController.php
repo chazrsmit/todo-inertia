e519a44473b28187a57b1c238eed46fb057c6793
Charlotte <smit-charlotte@outlook.com>
2025-09-08 20:21:41 +0200

    fonction store tache

--- app/Http/Controllers/TacheController.php
+++ app/Http/Controllers/TacheController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tache;
 use Illuminate\Http\Request;
 
 class TacheController extends Controller
@@ -12,7 +13,16 @@ class TacheController extends Controller
     // on va dire qu'automatiquement, quand une tâche est créée, on va lui attribuer les filtres avec id 1 et 2 (avec attach) > filtres->id[1,2]
 
     public function store(Request $request) {
-        
+        $tache = new Tache();
+
+        $tache->nom = $request->nom;
+        // on sauve la tâche avant d'établir la relation many 2 many (car la fonction attach a besoin de l'id de la tâche)
+        $tache->save();
+
+        // on va donc utiliser la fonction attach() pour attribuer automatiquement les filtres_id 1 et 2.
+        // cette fonction permet d'associer des données dans une relation many 2 many
+        $tache->filtres()->attach([1,2]);
+
     }
 
     // Modifier une tâche :
